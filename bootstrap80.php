947d45e18c48ae1ccce23ff1add57ff4b0c031cd
Nicolas Grekas <nicolas.grekas@gmail.com>
2020-12-26 12:39:55 +0100

    Verify the signature of polyfills on PHP >= 8

--- bootstrap80.php
+++ bootstrap80.php
@@ -11,14 +11,6 @@
 
 use Symfony\Polyfill\Intl\Idn as p;
 
-if (extension_loaded('intl')) {
-    return;
-}
-
-if (PHP_VERSION_ID >= 80000) {
-    return require __DIR__.'/bootstrap80.php';
-}
-
 if (!defined('U_IDNA_PROHIBITED_ERROR')) {
     define('U_IDNA_PROHIBITED_ERROR', 66560);
 }
@@ -128,18 +120,9 @@ if (!defined('IDNA_ERROR_CONTEXTJ')) {
     define('IDNA_ERROR_CONTEXTJ', 4096);
 }
 
-if (PHP_VERSION_ID < 70400) {
-    if (!function_exists('idn_to_ascii')) {
-        function idn_to_ascii($domain, $flags = 0, $variant = INTL_IDNA_VARIANT_2003, &$idna_info = null) { return p\Idn::idn_to_ascii($domain, $flags, $variant, $idna_info); }
-    }
-    if (!function_exists('idn_to_utf8')) {
-        function idn_to_utf8($domain, $flags = 0, $variant = INTL_IDNA_VARIANT_2003, &$idna_info = null) { return p\Idn::idn_to_utf8($domain, $flags, $variant, $idna_info); }
-    }
-} else {
-    if (!function_exists('idn_to_ascii')) {
-        function idn_to_ascii($domain, $flags = 0, $variant = INTL_IDNA_VARIANT_UTS46, &$idna_info = null) { return p\Idn::idn_to_ascii($domain, $flags, $variant, $idna_info); }
-    }
-    if (!function_exists('idn_to_utf8')) {
-        function idn_to_utf8($domain, $flags = 0, $variant = INTL_IDNA_VARIANT_UTS46, &$idna_info = null) { return p\Idn::idn_to_utf8($domain, $flags, $variant, $idna_info); }
-    }
+if (!function_exists('idn_to_ascii')) {
+    function idn_to_ascii(string $domain, int $flags = 0, int $variant = INTL_IDNA_VARIANT_UTS46, &$idna_info = null): string|false { return p\Idn::idn_to_ascii($domain, $flags, $variant, $idna_info); }
+}
+if (!function_exists('idn_to_utf8')) {
+    function idn_to_utf8(string $domain, int $flags = 0, int $variant = INTL_IDNA_VARIANT_UTS46, &$idna_info = null): string|false { return p\Idn::idn_to_utf8($domain, $flags, $variant, $idna_info); }
 }
